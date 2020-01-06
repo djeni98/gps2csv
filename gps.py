d51f9cc8c44c624e8b6c6b6d3b8408c7574ac8ef
djeni98 <djeniferrenata@yahoo.com.br>
2020-01-06 15:43:56 -0300

    Parse Command Line Arguments

--- gps.py
+++ gps.py
@@ -1,10 +1,7 @@
-import pynmea2, sys
+import pynmea2, sys, getopt
 import pandas as pd
 
 import functions as fct
-
-filename = 'gps-total.nmea'
-output = 'out.csv'
 
 def addRowInDataFrame(raw_row, df):
     df_concat = []
@@ -33,56 +30,95 @@ def addRowInDataFrame(raw_row, df):
     except KeyError:
         return df
 
+help_str = ("Usage: gps.py -i <input> -o <output>\n"
+            "Convert .nmea to .csv\n"
+            "\n"
+            "  -h, --help   Print this help message and exit\n"
+            "  -i, --input  Read <input> file\n"
+            "  -o, --output Write processed data in <output> file\n"
+)
 
-e = []
-row = []
-first = None
-data = pd.DataFrame()
+def getCommandLineArgs(argv):
+    if len(argv) == 0:
+        return None
 
-total_lines = 0
+    cmdline = {}
 
-with open(filename, 'r') as f:
-    for line in f:
-        total_lines += 1
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["help", "input=", "output="])
+    except getopt.GetoptError:
+        print(help_str)
+        sys.exit(2)
 
-print('Input file: "{}" ({} lines)'.format(filename, total_lines))
-print('Output file: "{}"'.format(output))
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            print(help_str)
+            sys.exit()
+        elif opt in ('-i', '--input'):
+            cmdline['i'] = arg
+        elif opt in ('-o', '--output'):
+            cmdline['o'] = arg
 
-print('')
+    if not ('i' in cmdline and 'o' in cmdline):
+        print(help_str)
+        sys.exit(2)
 
-with open(filename, 'r') as f:
-    i = 0
-    for line in f:
-        i += 1
-        print('Line {}/{}'.format(i, total_lines), end='\r')
-        try:
-            msg = pynmea2.parse(line)
-            if type(msg).__name__ != 'TXT':
-                if first == None:
-                    first = msg
-                elif type(msg) == type(first):
-                    data = addRowInDataFrame(row, data)
-                    row = []
+    return cmdline['i'], cmdline['o']
 
-                row.append(msg)
-        except:
-            e.append('{}: {}'.format(i, sys.exc_info()[1]))
+def main(filename='test.nmea', output='out.csv'):
+    e = []
+    row = []
+    first = None
+    data = pd.DataFrame()
 
-if len(row) > 1:
-    data = addRowInDataFrame(row, data)
+    total_lines = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            total_lines += 1
 
-print('')
-print('')
-print('Saving into "{}"'.format(output))
-print('')
+    print('Input file: "{}" ({} lines)'.format(filename, total_lines))
+    print('Output file: "{}"'.format(output))
 
-data.to_csv(output, index=False)
+    print('')
 
+    with open(filename, 'r') as f:
+        i = 0
+        for line in f:
+            i += 1
+            print('Line {}/{}'.format(i, total_lines), end='\r')
+            try:
+                msg = pynmea2.parse(line)
+                if type(msg).__name__ != 'TXT':
+                    if first == None:
+                        first = msg
+                    elif type(msg) == type(first):
+                        data = addRowInDataFrame(row, data)
+                        row = []
 
-if len(e) == 0:
-    print('Ok')
-else:
-    print('There were some errors')
-    for err in e:
-        print(err)
+                    row.append(msg)
+            except:
+                e.append('{}: {}'.format(i, sys.exc_info()[1]))
 
+    if len(row) > 1:
+        data = addRowInDataFrame(row, data)
+
+    print('')
+    print('')
+    print('Saving into "{}"'.format(output))
+    print('')
+
+    data.to_csv(output, index=False)
+
+    if len(e) == 0:
+        print('Ok')
+    else:
+        print('There were some errors')
+        for err in e:
+            print(err)
+
+if __name__ == '__main__':
+    cmdline = getCommandLineArgs(sys.argv[1:])
+    if cmdline:
+        main(cmdline[0], cmdline[1])
+    else:
+        main()
