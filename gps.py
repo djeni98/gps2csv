46d341f92f34f80633abf8887884d1924f97d58a
djeni98 <djeniferrenata@yahoo.com.br>
2020-01-06 12:12:27 -0300

    Write data to output file

--- gps.py
+++ gps.py
@@ -4,50 +4,77 @@ import numpy as np
 
 import functions as fct
 
+filename = 'test.nmea'
+output = 'out.csv'
+
 def addRowInDataFrame(raw_row, df):
+    df_concat = []
     for i, msg in enumerate(raw_row):
         mtype = type(msg).__name__
 
-        if fct.functions.has_key(mtype):
+        if mtype in fct.functions:
             getDataFunction = fct.functions[mtype]
 
             rows = getDataFunction(msg)
             cols = fct.columns[mtype]
 
-            print('-> {}: {}'.format(mtype, repr(cols)))
-            print(repr(rows))
-            # Create DataFrame or Series
+            df_concat.append(pd.DataFrame([rows], columns=cols))
         else:
             continue
 
+    df_row = pd.concat(df_concat, axis=1)
+    valid = df_row.get('Valid?')[0]
+
     # Concat row in DataFrame if it is valid
-    return None
+    if valid:
+        df = pd.concat([df, df_row])
+
+    return df
 
 
-v = []
 e = []
-first = None
 row = []
-df = None
+first = None
+data = pd.DataFrame()
 
-with open('test.nmea', 'r') as f:
+total_lines = 0
+
+with open(filename, 'r') as f:
     for line in f:
+        total_lines += 1
+
+with open(filename, 'r') as f:
+    i = 0
+    for line in f:
+        i += 1
+        print('Line {}/{}'.format(i, total_lines), end='\r')
         try:
             msg = pynmea2.parse(line)
             if type(msg).__name__ == 'TXT':
-                print(repr(msg))
+                pass
+                # print('Ignoring TXT message', end='\r')
+                # print(repr(msg))
             else:
-                v.append(msg)
                 if first == None:
                     first = msg
-                elif msg == first:
-                    df = addRowInDataFrame(row, df)
+                elif type(msg) == type(first):
+                    data = addRowInDataFrame(row, data)
                     row = []
-                row.append(msg) 
+                row.append(msg)
         except:
             e.append(sys.exc_info()[0])
 
-if len(row) > 0:
-    df = addRowInDataFrame(row, df)
+if len(row) > 1:
+    data = addRowInDataFrame(row, data)
 
-print("Ok")
+data.to_csv(output, index=False)
+
+print('')
+
+if len(e) == 0:
+    print('Ok')
+else:
+    print('There were some errors')
+    for err in e:
+        print(repr(err))
+
